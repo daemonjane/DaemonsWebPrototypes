bfbb71b133675a67cd7b73bd750e35942ef98b83
daemonjane <vxnce.jane@gmail.com>
2026-06-10 16:44:59 +0300

    feat: add ImageWithFallback component for missing assets

---
+++ vue-storefront/src/components/ImageWithFallback.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  src: String,
+  alt: String,
+  class: String
+})
+const error = ref(false)
+function onError() { error.value = true }
+</script>
+
+<template>
+  <img v-if="!error" :src="src" :alt="alt" :class="class" @error="onError" />
+  <div v-else :class="class" class="bg-slate-700 flex items-center justify-center text-slate-500 text-sm">
+    {{ alt }}
+  </div>
+</template>
