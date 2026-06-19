90b1f5aa39ffe035ba72791975bf00e399beaae1
daemonjane <vxnce.jane@gmail.com>
2026-06-19 11:23:10 +0300

    feat: home view queries latest 5 tasks and passes to template

--- backend/website/views.py
+++ backend/website/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Task
+
 
 def home(request):
-    return render(request, "website/home.html")
+    recent_tasks = Task.objects.all()[:5]
+    return render(request, "website/home.html", {"recent_tasks": recent_tasks})
