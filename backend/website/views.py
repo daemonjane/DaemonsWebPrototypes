5909b16517cc8c1981fa1453fc0a17589ce12eda
daemonjane <vxnce.jane@gmail.com>
2026-06-19 12:06:46 +0300

    chore: import get_object_or_404 for detail/edit/delete views

--- backend/website/views.py
+++ backend/website/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import ContactForm, TaskForm
 from .models import Task
