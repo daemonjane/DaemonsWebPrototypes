80df315981ef00b29950175205cf264e0239d494
daemonjane <vxnce.jane@gmail.com>
2026-06-19 11:24:09 +0300

    feat: add contact and contact/thanks URL routes

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("contact/", views.contact, name="contact"),
+    path("contact/thanks/", views.contact_thanks, name="contact_thanks"),
 ]
