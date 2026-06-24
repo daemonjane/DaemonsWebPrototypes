56d4a4a4490e40d61fb30b8c9a17eec2826948bc
daemonjane <vxnce.jane@gmail.com>
2026-06-24 15:29:43 +0300

    Configure ManifestStaticFilesStorage for static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,6 +142,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"},
+}
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
