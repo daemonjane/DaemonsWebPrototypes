22225ed2f4f7ae833b042f66bc6d4fc1c9423f71
daemonjane <vxnce.jane@gmail.com>
2026-06-29 02:04:57 +0300

    fix: failing test assertion (1+1 != 1) in test_1.py

--- backend/api/tests/test_1.py
+++ backend/api/tests/test_1.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-"""Tests for test_1."""
 
 class TestSuite1(TestCase):
     def test_trivial(self):
-        self.assertEqual(1 + 1, 1)
+        self.assertEqual(1 + 1, 2)
